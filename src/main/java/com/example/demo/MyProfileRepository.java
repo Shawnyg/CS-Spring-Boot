d945c56b09ab57a982ed96d6dd9ecb4135d8e2ea
Shawnyg <shawnyg@no.com>
2020-11-02 16:41:50 -0500

    deletions

--- src/main/java/com/example/demo/MyProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.User;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called MyProfileRepository
-public interface MyProfileRepository extends CrudRepository<User, Integer> {
-
-    User findByName(String username);
-}
