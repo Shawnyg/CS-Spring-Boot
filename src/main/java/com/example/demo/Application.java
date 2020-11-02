0d1e6b6152112795d844e50a0028182e4231d22c
Shawnyg <shawnyg@no.com>
2020-11-02 18:50:21 -0500

    edits

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyProfileApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyProfileApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
